c39d4476eee753538360cddc2f935e41d92cb9ce
Mordy <imky171@gmail.com>
2021-03-10 22:36:32 -0500

    Movies

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, url_for, redirect
 import covid
 import movies
 
@@ -17,11 +17,51 @@ def covidAPI():
 
 
 @app.route('/apis/movies')
-def moviesAPI():
-    results = movies.get_most_popular()
-    for result in results:
-        result['title'], season = result['title'].split(':')
-    return render_template('movies.html', res=results, season=season)
+def moviesHome():
+    return render_template('moviesHome.html')
+
+
+@app.route('/apis/movies/<category>')
+def moviesAPI(category):
+    if category == 'action':
+        result = movies.get_action()
+        results = []
+        for item in result[:10]:
+            if '(' in item['title']:
+                item['title'], junk = item['title'].split('(')
+                del(junk)
+            results.append(item)
+        return render_template('movies.html', res=results, category='Action Movies')
+    elif category == 'comedy':
+        result = movies.get_comedy()
+        results = []
+        for item in result[:10]:
+            if '(' in item['title']:
+                item['title'], junk = item['title'].split('(')
+                del(junk)
+            results.append(item)
+        return render_template('movies.html', res=results, category='Comedy Movies')
+    elif category == 'kids_and_family':
+        result = movies.get_kids_and_family()
+        results = []
+        for item in result[:10]:
+            if '(' in item['title']:
+                item['title'], junk = item['title'].split('(')
+                del(junk)
+            results.append(item)
+        return render_template('movies.html', res=results, category='Kids and Family Movies')
+    elif category == 'most_popular':
+        result = movies.get_most_popular()
+        results = []
+        for item in result[:10]:
+            if '(' in item['title']:
+                item['title'], junk = item['title'].split('(')
+                del(junk)
+            results.append(item)
+        return render_template('movies.html', res=results, category='Popular')
+
+    else:
+        return redirect(url_for('moviesHome'))
 
 
 if __name__ == '__main__':
--- movies.py
+++ movies.py
@@ -1,6 +1,26 @@
-from rotten_tomatoes_client import RottenTomatoesClient, TvBrowsingCategory
+from rotten_tomatoes_client import RottenTomatoesClient, TvBrowsingCategory, MovieBrowsingQuery, Service, Genre, SortBy, MovieBrowsingCategory
+
 
 def get_most_popular():
     results = RottenTomatoesClient.browse_tv_shows(
         category=TvBrowsingCategory.most_popular)
     return results['results']
+
+def get_action():
+    query = MovieBrowsingQuery(
+        services=[Service.netflix, Service.amazon_prime, Service.amazon, Service.vudu, Service.hbo_go], category=MovieBrowsingCategory.all_dvd_and_streaming, sort_by=SortBy.popularity, genres=[Genre.action])
+    result = RottenTomatoesClient.browse_movies(query)
+    result = result['results']
+    return result
+def get_comedy():
+    query = MovieBrowsingQuery(
+        services=[Service.netflix, Service.amazon_prime, Service.amazon, Service.vudu, Service.hbo_go], category=MovieBrowsingCategory.all_dvd_and_streaming, sort_by=SortBy.popularity, genres=[Genre.comedy])
+    result = RottenTomatoesClient.browse_movies(query)
+    result = result['results']
+    return result
+def get_kids_and_family():
+    query = MovieBrowsingQuery(
+        services=[Service.netflix, Service.amazon_prime, Service.amazon, Service.vudu, Service.hbo_go], category=MovieBrowsingCategory.all_dvd_and_streaming, sort_by=SortBy.popularity, genres=[Genre.kids_and_family])
+    result = RottenTomatoesClient.browse_movies(query)
+    result = result['results']
+    return result
